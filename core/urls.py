ac5656a165a3b8d414856ac6a194d135e98a8801
Ajsteph89 <ajsteph89@gmail.com>
2022-09-08 16:07:12 -0400

    basic CRUD completed

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('habit/add/', views.add_habit, name='add_habit'),
     path('habit_detail/<int:pk>', views.habit_detail, name='habit_detail'),
     path('record/new/<int:pk>', views.add_record, name='add_record'),
+    path('habit/edit/<int:pk>', views.edit_habit, name='edit_habit'),
+    path('delete_habit/<int:pk>', views.delete_habit, name='delete_habit'),
+    path('record/edit/<int:pk1>/<int:pk2>', views.edit_record, name='edit_record'),
+    path('delete_record/<int:pk1>/<int:pk2>', views.delete_record, name='delete_record'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from core.models import Habit
+from core.models import DailyRecord, Habit
 from .forms import HabitForm, RecordForm
 
 # Create your views here.
@@ -46,3 +46,42 @@ def add_record(request, pk):
     else:
         record_form = RecordForm()
     return render(request, 'core/add_record.html', {'record_form': record_form})
+
+def edit_habit(request, pk):
+    habit = get_object_or_404(Habit, pk=pk)
+    if request.method == "POST":
+        habit_form = HabitForm(request.POST, instance=habit)
+        if habit_form.is_valid():
+            habit = habit_form.save(commit=False)
+            habit.save()
+            return redirect('list_habits')
+    else:
+        habit_form = HabitForm(instance=habit)
+    return render(request, 'core/edit_habit.html', {'habit_form': habit_form})
+
+def delete_habit(request, pk):
+    habit = get_object_or_404(Habit, pk=pk)
+    habit.delete()
+    return redirect('list_habits')
+
+
+def edit_record(request, pk1, pk2):
+    record = get_object_or_404(DailyRecord, pk=pk2)
+    habit = get_object_or_404(Habit, pk=pk1)
+    if request.method == "POST":
+        record_form = RecordForm(request.POST, instance=record)
+        if record_form.is_valid():
+            record = record_form.save(commit=False)
+            record.habit = habit
+            record.user = request.user
+            record.save()
+            return redirect('habit_detail', pk=pk1)
+    else:
+        record_form = RecordForm(instance=record)
+    return render(request, 'core/edit_record.html', {'record_form': record_form})
+
+def delete_record(request, pk1, pk2):
+    record = get_object_or_404(DailyRecord, pk=pk2)
+    record.delete()
+    habit = get_object_or_404(Habit, pk=pk1)
+    return redirect('habit_detail', pk=pk1)
